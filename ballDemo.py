5635fd138c88b7d22eff49b5af8bfa99fa94bdd7
nathan.carbee <nathan.carbee@kearsarge.org>
2014-12-04 09:00:37 -0500

    2 Player!

--- ballDemo.py
+++ ballDemo.py
@@ -18,7 +18,8 @@ bgR = bgI.get_rect()
 
 screen = pygame.display.set_mode(size)
 
-player = PlayerBall([width/2, height/2])
+player1 = PlayerBall([width/3, height/3])
+player2 = PlayerBall([2*width/3, 2*height/3])
 
 balls = []
 balls += [Ball("images/Ball/ball.png", [4,5], [100, 125])]
@@ -27,23 +28,24 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w or event.key == pygame.K_UP:
-                player.go("up")
-            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                player.go("right")
-            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                player.go("down")
-            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                player.go("left")
+            if event.key == pygame.K_w:
+                player1.go("up")
+            if event.key == pygame.K_d:
+                player1.go("right")
+            if event.key == pygame.K_s:
+                player1.go("down")
+            if event.key == pygame.K_a:
+                player1.go("left")
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_w or event.key == pygame.K_UP:
-                player.go("stop up")
-            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                player.go("stop right")
-            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                player.go("stop down")
-            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                player.go("stop left")
+            if event.key == pygame.K_w:
+                player1.go("stop up")
+            if event.key == pygame.K_d:
+                player1.go("stop right")
+            if event.key == pygame.K_s:
+                player1.go("stop down")
+            if event.key == pygame.K_a:
+                player1.go("stop left")
+                
         
     if len(balls) < 10:
         if random.randint(0, .25*60) == 0:
@@ -59,14 +61,18 @@ while True:
                       ]
   
   
-    player.update(width, height)
+    player1.update(width, height)
+    player2.update(width, height)
     for ball in balls:
         ball.update(width, height)
+        
+    
         
     for bully in balls:
         for victem in balls:
             bully.collideBall(victem)
-            bully.collidePlayer(player)
+            bully.collidePlayer(player1)
+            bully.collidePlayer(player2)
     
     for ball in balls:
         if not ball.living:
@@ -77,7 +83,9 @@ while True:
     screen.blit(bgI, bgR)
     for ball in balls:
         screen.blit(ball.image, ball.rect)
-    screen.blit(player.image, player.rect)
+    screen.blit(player1.image, player1.rect)
+    screen.blit(player2.image, player2.rect)
+    (player2, -50, 50) 
     pygame.display.flip()
     clock.tick(60)
         
