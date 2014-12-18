be5f36c217a7ccb9e9e946b4a4b80776b5b2267b
nathan.carbee <nathan.carbee@kearsarge.org>
2014-12-18 08:57:49 -0500

    Fixed player Images Fixed player rect Walls work Changed move so that speed is calculted on move rather than update

--- CNYgame.py
+++ CNYgame.py
@@ -1,8 +1,7 @@
 import pygame, sys, random
-from HUD import *
 from Ball import Ball
 from PlayerBall import PlayerBall
-
+from Wall import Wall
 
 
 pygame.init()
@@ -10,13 +9,29 @@ pygame.init()
 clock = pygame.time.Clock()
 
 
-width = 960
-height = 600
+width = 955
+height = 610
 size = width, height
 
 bgColor = r,g,b = 100, 30, 234
 bgI = pygame.image.load("map1.png")
 bgR = bgI.get_rect()
+
+walls = [Wall([13, 12], [931, 28]),
+         Wall([13, 12], [26, 593]),
+         Wall([21, 254], [90, 271]),
+         Wall([163, 255], [577, 268]),
+         Wall([270, 256], [285, 576]),
+         Wall([562, 262], [576, 349]),
+         Wall([23, 157], [254, 173]),
+         Wall([317, 157], [752, 175]),
+         Wall([739, 157], [755, 351]),
+         Wall([512, 28], [529, 158]),
+         Wall([913, 14], [931, 592]),
+         Wall([560, 416], [576, 577]),
+         Wall([739, 415], [755, 575])]
+         
+    
 
 screen = pygame.display.set_mode(size)
 
@@ -24,9 +39,7 @@ player1 = PlayerBall([width/3, height/3])
 player2 = PlayerBall([2*width/3, 2*height/3])
 
 balls = []
-balls += [Ball("images/Ball/ball.png", [4,5], [100, 125])]
- 
-title = Text([height/4, width/8], "Hello HUD!!", 20)
+balls += [Ball("images/Ball/crabman.png", [4,5], [100, 125])]
 
 while True:
     for event in pygame.event.get():
@@ -69,13 +82,13 @@ while True:
                 player2.go("stop left")
                 
         
-    if len(balls) < 10:
+    if len(balls) < 5:
         if random.randint(0, .25*60) == 0:
             balls += [Ball("images/Ball/spinnything.png",
                       [random.randint(0,10), random.randint(0,10)],
                       [random.randint(100, width-100), random.randint(100, height-100)])
                       ]
-    if len(balls) < 10:
+    if len(balls) < 5:
         if random.randint(0, .25*60) == 0:
             balls += [Ball("images/Ball/crabman.png",
                       [random.randint(0,10), random.randint(0,10)],
@@ -88,7 +101,11 @@ while True:
     for ball in balls:
         ball.update(width, height)
         
-    
+    for wall in walls:
+        player1.collideWall(wall)
+        player2.collideWall(wall)
+     #   for ball in ball:
+      #      ball.collideWall(wall)
         
     for bully in balls:
         for victem in balls:
@@ -105,6 +122,8 @@ while True:
     screen.blit(bgI, bgR)
     for ball in balls:
         screen.blit(ball.image, ball.rect)
+    for wall in walls:
+        screen.blit(wall.image, wall.rect)
     screen.blit(player1.image, player1.rect)
     screen.blit(player2.image, player2.rect)
     pygame.display.flip()
---
+++ Wall.py
@@ -0,0 +1,11 @@
+#Code from Riley Skinner and Brian Calagahan
+#https://code.google.com/p/thetempleoflobsterman/
+import pygame, math
+
+class Wall():
+	def __init__(self, tl, br):
+		self.image = pygame.image.load("images/Wall/Wall.png")
+		width = br[0] - tl[0]
+		height = br[1] - tl[1]
+		self.image = pygame.transform.scale(self.image, [width, height])	
+		self.rect = self.image.get_rect(topleft = tl)
